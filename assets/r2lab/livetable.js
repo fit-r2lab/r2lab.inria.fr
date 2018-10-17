06884aa9340ad488fc8373b59ebaf9ef6e1d94d1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-10-17 10:27:38 +0200

    only replace tabs with spaces

--- assets/r2lab/livetable.js
+++ assets/r2lab/livetable.js
@@ -30,28 +30,28 @@ let livetable_options = {
 class LiveTableNode extends LiveColumnsNode {
 
     constructor(id) {
-	super(id);
+        super(id);
 
-	this.cells_data = [
-	    [ span_html(id, 'badge pointer'), '' ],	// id
-	    undefined,				// avail
-	    undefined,				// on/off
-	    undefined,				// usrp-on-off
-	    undefined,				// ping
-	    undefined,				// ssh
-	    undefined,				// os_release
-	    undefined,				// uname
-	    undefined				// image_radical
-	];
+        this.cells_data = [
+            [ span_html(id, 'badge pointer'), '' ],	// id
+            undefined,				// avail
+            undefined,				// on/off
+            undefined,				// usrp-on-off
+            undefined,				// ping
+            undefined,				// ssh
+            undefined,				// os_release
+            undefined,				// uname
+            undefined,				// image_radical
+        ];
     }
 
     // nodes worth being followed when clicking on the table banner
     is_worth() {
-	return (   this.cmc_on_off == 'on'
-		   || this.usrp_on_off == 'on'
-		   || this.control_ping == 'on'
-		   || this.control_ssh == 'on' )
-	    && this.available != 'ko';
+        return (   this.cmc_on_off == 'on'
+                || this.usrp_on_off == 'on'
+                || this.control_ping == 'on'
+                || this.control_ssh == 'on' )
+                && this.available != 'ko';
     }
 
 
@@ -60,52 +60,52 @@ class LiveTableNode extends LiveColumnsNode {
     // that will contain a list of ( html_text, class )
     // used by the d3 mechanism to update the <td> elements in the row
     compute_cells_data () {
-	let col = 1
-	// available
-	this.cells_data[col++] = this.cell_available();
-	// on/off
-	this.cells_data[col++] = this.cell_on_off();
-	// usrp
-	this.cells_data[col++] = this.cell_sdr(true);
-	// ping
-	this.cells_data[col++] =
-	    (this.control_ping == 'on')
-	    ? [ span_html('', 'fa fa-link'), 'ok' ]
-	    : [ span_html('', 'fa fa-unlink'), 'ko' ];
-	// ssh
-	this.cells_data[col++] =
-	    this.control_ssh == 'on' ? [ span_html('', 'fa fa-circle'), 'ok' ]
-	    : [ span_html('', 'fa fa-circle-o'), 'ko' ];
-	//
-	this.cells_data[col++] = this.cell_release(this.os_release);
-	this.cells_data[col++] = this.cell_uname(this.uname);
-	this.cells_data[col++] = this.cell_image(this.image_radical);
+        let col = 1
+        // available
+        this.cells_data[col++] = this.cell_available();
+        // on/off
+        this.cells_data[col++] = this.cell_on_off();
+        // usrp
+        this.cells_data[col++] = this.cell_sdr(true);
+        // ping
+        this.cells_data[col++] =
+            (this.control_ping == 'on')
+            ? [ span_html('', 'fa fa-link'), 'ok' ]
+            : [ span_html('', 'fa fa-unlink'), 'ko' ];
+        // ssh
+        this.cells_data[col++] =
+            this.control_ssh == 'on' ? [ span_html('', 'fa fa-circle'), 'ok' ]
+            : [ span_html('', 'fa fa-circle-o'), 'ko' ];
+        //
+        this.cells_data[col++] = this.cell_release(this.os_release);
+        this.cells_data[col++] = this.cell_uname(this.uname);
+        this.cells_data[col++] = this.cell_image(this.image_radical);
     }
 
     cell_release(os_release) {
-	// use a single css class for now
-	let klass = 'os';
-	if (os_release == undefined)
-	    return [ "n/a", klass ];
-	if (os_release.startsWith('fedora'))
-	    return [ livetable_options.fedora_badge + ' ' + os_release + ' ', klass ];
-	else if (os_release.startsWith('ubuntu'))
-	    return [ livetable_options.ubuntu_badge + ' ' + os_release + ' ', klass ];
-	else if (os_release == 'other')
-	    return [ livetable_options.other_badge + ' (ssh OK)', klass ];
-	else
-	    return [ 'N/A', klass ];
+        // use a single css class for now
+        let klass = 'os';
+        if (os_release == undefined)
+            return [ "n/a", klass ];
+        if (os_release.startsWith('fedora'))
+            return [ livetable_options.fedora_badge + ' ' + os_release + ' ', klass ];
+        else if (os_release.startsWith('ubuntu'))
+            return [ livetable_options.ubuntu_badge + ' ' + os_release + ' ', klass ];
+        else if (os_release == 'other')
+            return [ livetable_options.other_badge + ' (ssh OK)', klass ];
+        else
+            return [ 'N/A', klass ];
     }
 
     cell_uname(uname) {
-	return [ uname, 'os' ];
+        return [ uname, 'os' ];
     }
-    
+
     cell_image(image_radical) {
-	let klass = 'image';
-	if (image_radical == undefined)
-	    return [ "n/a", klass ];
-	return [ image_radical, klass ];
+        let klass = 'image';
+        if (image_radical == undefined)
+            return [ "n/a", klass ];
+        return [ image_radical, klass ];
     }
 }
 
@@ -113,27 +113,27 @@ class LiveTableNode extends LiveColumnsNode {
 class LiveTable extends LiveColumns{
 
     constructor(domid) {
-	super();
-	this.domid = domid;
+        super();
+        this.domid = domid;
     }
 
-    
+
     init_headers(header) {
-	header.append('th').html('node');
-	header.append('th').html('avail.');
-	header.append('th').html('on/off');
-	header.append('th').html('sdr');
-	header.append('th').html('ping');
-	header.append('th').html('ssh');
-	header.append('th').html('last O.S.');
-	header.append('th').html('last uname');
-	header.append('th').html('last image');
+        header.append('th').html('node');
+        header.append('th').html('avail.');
+        header.append('th').html('on/off');
+        header.append('th').html('sdr');
+        header.append('th').html('ping');
+        header.append('th').html('ssh');
+        header.append('th').html('last O.S.');
+        header.append('th').html('last uname');
+        header.append('th').html('last image');
     }
 
     init_nodes() {
-	for (let i=0; i < livecolumns_options.nb_nodes; i++) {
-	    this.nodes[i] = new LiveTableNode(i+1);
-	}
+        for (let i=0; i < livecolumns_options.nb_nodes; i++) {
+            this.nodes[i] = new LiveTableNode(i+1);
+        }
     }
 }
 
