cacc4329203f56f38210b55d4b30f971d669eb42
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-03-08 12:35:19 +0100

    move to django 4 was incomplete

--- md/urls.py
+++ md/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 # xxx not used in fact, see r2lab/urls.py that manages this directly
 urlpatterns = [
-#    url('^(?P<markdown_file>.*)$', views.markdown_page, name='show'),
+#    re_path('^(?P<markdown_file>.*)$', views.markdown_page, name='show'),
     ]
--- r2lab/urls.py
+++ r2lab/urls.py
@@ -10,10 +10,10 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
+    1. Import the include() function: from django.urls import include, re_path
     2. Add a URL to urlpatterns:  re_path(r'^news/', include('news.urls'))
 """
-from django.conf.urls import include, re_path
+from django.urls import include, re_path
 from django.contrib import admin
 
 from django.conf import settings
