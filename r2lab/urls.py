bc9655110658131dc99266d1af29a7fccb43aa9e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-03-08 07:44:46 +0100

    use re_path for django 4

--- r2lab/urls.py
+++ r2lab/urls.py
@@ -5,15 +5,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^news/', include('news.urls'))
+    2. Add a URL to urlpatterns:  re_path(r'^news/', include('news.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.contrib import admin
 
 from django.conf import settings
@@ -33,16 +33,16 @@ BASE = Path(settings.BASE_DIR)
 
 urlpatterns = [
     # default: empty or just / -> md/index.md
-    url(r'^(/)?$', RedirectView.as_view(url='/index.md', permanent=False)),
+    re_path(r'^(/)?$', RedirectView.as_view(url='/index.md', permanent=False)),
     # no subdir
-    url(r'^(?P<markdown_file>[^/]*)$', md.views.markdown_page),
-    url(r'^md/(?P<markdown_file>.*)$', md.views.markdown_page),
-    url(r'^login/', mfauth.views.Login.as_view()),
-    url(r'^logout/', mfauth.views.Logout.as_view()),
-    url(r'^leases/(?P<verb>(add|update|delete))', leases.views.LeasesProxy.as_view()),
-    url(r'^slices/(?P<verb>(get|renew))', slices.views.SlicesProxy.as_view()),
-    url(r'^users/(?P<verb>(get|renew))', users.views.UsersProxy.as_view()),
-    url(r'^keys/(?P<verb>(get|add|delete))', keys.views.KeysProxy.as_view()),
+    re_path(r'^(?P<markdown_file>[^/]*)$', md.views.markdown_page),
+    re_path(r'^md/(?P<markdown_file>.*)$', md.views.markdown_page),
+    re_path(r'^login/', mfauth.views.Login.as_view()),
+    re_path(r'^logout/', mfauth.views.Logout.as_view()),
+    re_path(r'^leases/(?P<verb>(add|update|delete))', leases.views.LeasesProxy.as_view()),
+    re_path(r'^slices/(?P<verb>(get|renew))', slices.views.SlicesProxy.as_view()),
+    re_path(r'^users/(?P<verb>(get|renew))', users.views.UsersProxy.as_view()),
+    re_path(r'^keys/(?P<verb>(get|add|delete))', keys.views.KeysProxy.as_view()),
 ]
 urlpatterns.extend(static('/assets/', document_root=str(BASE / 'assets/')))
 urlpatterns.extend(static('/raw/', document_root=str(BASE / 'raw/')))
