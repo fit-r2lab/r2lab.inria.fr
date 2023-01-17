10a4e7ec44c718ccc8cd71cffbe018abeb7b5e7a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-17 15:09:35 +0100

    colons in liveslices

--- assets/r2lab/liveslices.js
+++ assets/r2lab/liveslices.js
@@ -4,99 +4,99 @@
 /*global $  moment*/
 /*global r2lab_accounts */
 
-"use strict";
+"use strict"
 
-import {load_css} from "/assets/r2lab/load-css.js";
-load_css("/assets/r2lab/liveslices.css");
+import {load_css} from "/assets/r2lab/load-css.js"
+load_css("/assets/r2lab/liveslices.css")
 
-import {post_xhttp_django} from "/assets/r2lab/xhttp-django.js";
+import {post_xhttp_django} from "/assets/r2lab/xhttp-django.js"
 
 $(function() {
 
     function normalize_id(name){
-        let new_name = name;
-        new_name = name.replace(/[_\s]/g, '-').replace(/[^a-z0-9-\s]/gi, '');
+        let new_name = name
+        new_name = name.replace(/[_\s]/g, '-').replace(/[^a-z0-9-\s]/gi, '')
         return new_name
     }
 
 
     function send_message(msg, type){
-        let cls   = 'danger';
+        let cls   = 'danger'
         let title = 'Ooops!'
         if(type == 'info'){
-            cls   = 'info';
+            cls   = 'info'
             title = 'Info:'
         }
         if(type == 'attention'){
-            cls   = 'warning';
+            cls   = 'warning'
             title = 'Attention:'
         }
         if(type == 'success'){
-            cls   = 'success';
+            cls   = 'success'
             title = 'Yep!'
         }
-        $('html,body').animate({'scrollTop' : 0}, 400);
-        $('#messages').removeClass().addClass(`alert alert-${cls}`);
-        $('#messages').html(`<strong>${title}</strong> ${msg}`);
-        $('#messages').fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+        $('html,body').animate({'scrollTop' : 0}, 400)
+        $('#messages').removeClass().addClass(`alert alert-${cls}`)
+        $('#messages').html(`<strong>${title}</strong> ${msg}`)
+        $('#messages').fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200)
     }
 
 
     function is_past_date(date){
-        let past = false;
+        let past = false
         if(moment().diff(date, 'minutes') > 0 && date != null){
-            past = true;
+            past = true
         }
-        return past;
+        return past
     }
 
 
     let get_slices = function(id, names) {
-        let body = $(`#${id}`);
+        let body = $(`#${id}`)
         body.html(
             "<div class='row slice-header'>"
           + "<div class='col-md-6'>Name</div>"
           + "<div class='col-md-4'>Expiration Date</div>"
           + "<div class='col-md-2'>Renew</div>"
-          + "</div>");
+          + "</div>")
         $.each(names, function(index, value){
 
-            let request = {};
-            request['names'] = [value];
+            let request = {}
+            request['names'] = [value]
 
             post_xhttp_django('/slices/get', request, function(xhttp) {
 
                 if ( ! (xhttp.readyState == 4 && xhttp.status == 200))
-                    return;
-                let responses = JSON.parse(xhttp.responseText);
+                    return
+                let responses = JSON.parse(xhttp.responseText)
                 if (responses.length <= 0)
-                    return;
+                    return
 
                 let slice_manage_invitation = '\
 One or more of your slices has expired. \
 <a href="#" data-toggle="modal" data-target="#slices_keys_modal">\
-Click here to renew it!</a>';
+Click here to renew it!</a>'
                 for (let i = 0; i < responses.length; i++) {
 
-                    let response   = responses[i];
-                    let slicename  = response['name'];
-                    let normal_id  = normalize_id(slicename);
-                    let expiration = response['valid_until'];
-                    let closed     = response['closed_at'];
-                    //let expiration = '2016-01-22T09:25:31Z';
+                    let response   = responses[i]
+                    let slicename  = response['name']
+                    let normal_id  = normalize_id(slicename)
+                    let expiration = response['valid_until']
+                    let closed     = response['closed_at']
+                    //let expiration = '2016-01-22T09:25:31Z'
 
-                    let s_class   = 'in-green';
-                    let s_id = `renew-slice-${normal_id}`;
-                    let s_icon = `<span class='fa fa-refresh in-blue' id='${s_id}'>`;
-                    let the_date  = moment(expiration).format("YYYY-MM-DD HH:mm");
+                    let s_class   = 'in-green'
+                    let s_id = `renew-slice-${normal_id}`
+                    let s_icon = `<span class='fa fa-refresh in-blue' id='${s_id}'>`
+                    let the_date  = moment(expiration).format("YYYY-MM-DD HH:mm")
                     if (is_past_date(expiration) || is_past_date(closed)){
-                        send_message(slice_manage_invitation, 'attention');
+                        send_message(slice_manage_invitation, 'attention')
 
                         if (is_past_date(closed)){
-                            the_date  = moment(closed).format("YYYY-MM-DD HH:mm");
+                            the_date  = moment(closed).format("YYYY-MM-DD HH:mm")
                         }
 
-                        s_class   = 'in-red';
+                        s_class   = 'in-red'
                     }
 
                     $(body).append(
@@ -105,40 +105,40 @@ Click here to renew it!</a>';
                       + `<div class='col-md-4' id='timestamp-expire${normal_id}'>`
                       + `<span class='${s_class}'>${the_date}</span>`
                       + `</div>`
-                      + `<div class='col-md-2'>${s_icon}</div>`);
+                      + `<div class='col-md-2'>${s_icon}</div>`)
                     $(`#${s_id}`).click(function() {
-                        renew_slice(normalize_id(slicename), slicename)});
+                        renew_slice(normalize_id(slicename), slicename)})
                 }
             })
-        });
+        })
     }
 
 
     let renew_slice = function(element, slicename) {
         let request = {
             "name" : slicename,
-        };
+        }
         post_xhttp_django('/slices/renew', request, function(xhttp) {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
-                let answer = JSON.parse(xhttp.responseText);
-                // console.log("answer from /slices/renew");
-                // console.log(answer);
+                let answer = JSON.parse(xhttp.responseText)
+                // console.log("answer from /slices/renew")
+                // console.log(answer)
 
-                $('#timestamp-expire'+element).removeClass('in-red');
-                $('#timestamp-expire'+element).addClass('in-green');
-                $('#timestamp-expire'+element).toggle("pulsate").toggle("highlight");
-                $('#timestamp-expire'+element).html(moment(answer['valid_until']).format("YYYY-MM-DD HH:mm"));
+                $('#timestamp-expire'+element).removeClass('in-red')
+                $('#timestamp-expire'+element).addClass('in-green')
+                $('#timestamp-expire'+element).toggle("pulsate").toggle("highlight")
+                $('#timestamp-expire'+element).html(moment(answer['valid_until']).format("YYYY-MM-DD HH:mm"))
             }
-        });
-    };
+        })
+    }
 
 
     function main(){
         let slicenames = r2lab_accounts.map(
-            function(account){return account['name'];});
-        get_slices("liveslices-container", slicenames);
+            function(account){return account['name']})
+        get_slices("liveslices-container", slicenames)
     }
 
 
-    main();
-});
+    main()
+})
