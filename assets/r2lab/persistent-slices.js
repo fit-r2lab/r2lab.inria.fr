5c272dc8d917fb26d554a344faf2ca91a74f2775
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-17 15:15:35 +0100

    colons in persistent-slices

--- assets/r2lab/persistent-slices.js
+++ assets/r2lab/persistent-slices.js
@@ -1,7 +1,7 @@
 /*
  * tools for managing current slice
  */
-"use strict";
+"use strict"
 
 /* for eslint */
 /*eslint no-unused-vars: ["error", { "varsIgnorePattern": "PersistentSlices" }]*/
@@ -10,7 +10,7 @@
 ////////// provide for array.first()
 if (!Array.prototype.first) {
   Array.prototype.first = function() {
-    return this[0];
+    return this[0]
   }
 }
 
@@ -23,7 +23,7 @@ class ColorsGenerator {
 
     constructor(category) {
         // capture incoming parameter
-        this.category = category;
+        this.category = category
 
         // hard-wire special cases
         this.hard_wired = {
@@ -32,8 +32,8 @@ class ColorsGenerator {
         }
 
         // special colors
-        this.foreign_color = "#999";
-        this.overflow_color = "#558";
+        this.foreign_color = "#999"
+        this.overflow_color = "#558"
 
         ////////////////////
         this.categories = {
@@ -57,13 +57,13 @@ class ColorsGenerator {
                 "#A41987", "#1B5DF8", "#7AAD82", "#8D72E4", "#323C89",
                 "#E4466b", "#DE9429", "#198C0F", "#AA322C", "#88713D",
             ],
-        };
+        }
 
         // in case category is wrong, use rainbow
-        this.colors = this.categories[this.category] || this.categories.rainbow;
+        this.colors = this.categories[this.category] || this.categories.rainbow
         // so a shallow copy
-        this.colors = this.colors.slice();
-        this._debug = false;
+        this.colors = this.colors.slice()
+        this._debug = false
     }
 
     debug(...args) {
@@ -75,20 +75,20 @@ class ColorsGenerator {
         if (slicename in this.hard_wired) {
             return this.hard_wired[slicename]
         } else if (! mine) {
-            return this.foreign_color;
+            return this.foreign_color
         }
-        let colors = this.colors;
+        let colors = this.colors
         if (colors.length > 0) {
-            let index = Math.floor(Math.random() * colors.length);
-            let color = colors[index];
-            this.debug(`random color: ${slicename} -> ${color}`);
+            let index = Math.floor(Math.random() * colors.length)
+            let color = colors[index]
+            this.debug(`random color: ${slicename} -> ${color}`)
             // remove color
-            colors.splice(index, 1);
-            return color;
+            colors.splice(index, 1)
+            return color
         } else {
             // xxx need to be smarter ?
-            console.warn("persistent_slices ran out of colors..");
-            return this.overflow_color;
+            console.warn("persistent_slices ran out of colors..")
+            return this.overflow_color
         }
     }
 
@@ -109,20 +109,20 @@ export class PersistentSlices {
     // at the beginning we only have a list of slice names
     constructor(r2lab_accounts, category) {
 
-        this.slicenames = r2lab_accounts.map(account => account.name);
+        this.slicenames = r2lab_accounts.map(account => account.name)
         // local storage uses this key internally
-        this.storage_key = 'r2lab_persistent_slices';
-        this.pslices = [];
-        this.colors_generator = new ColorsGenerator(category);
+        this.storage_key = 'r2lab_persistent_slices'
+        this.pslices = []
+        this.colors_generator = new ColorsGenerator(category)
 
         //// could be separated some place in some _init method..
         // read local storage
-        this._retrieve();
+        this._retrieve()
         // update pslices: add new slices, delete old ones
-        this._sync();
+        this._sync()
         // store again
-        this._store();
-        this._debug = false;
+        this._store()
+        this._debug = false
     }
 
     debug(...args) {
@@ -132,26 +132,26 @@ export class PersistentSlices {
 
     warn_missing_storage() {
         if (typeof(Storage) === undefined) {
-            console.warn("WARNING: PersistentSlices can't work properly - no persistent storage");
-            return true;
+            console.warn("WARNING: PersistentSlices can't work properly - no persistent storage")
+            return true
         }
     }
 
     // extract this.pslices from localStorage
     // fill this.pslices from this.slicenames if not possible
     _retrieve() {
-        let generator = this.colors_generator;
+        let generator = this.colors_generator
         try {
             /* with storage */
-            this.pslices = JSON.parse(localStorage.getItem(this.storage_key));
+            this.pslices = JSON.parse(localStorage.getItem(this.storage_key))
             if ( ! this.pslices) {
-                throw new Error();
+                throw new Error()
             }
             this.debug("retrieved from local storage", this.pslices)
         } catch(err) {
             if ( ! this.warn_missing_storage()) {
-                this.debug("Could not retrieve persistent slices..");
-                this.debug(err.stack);
+                this.debug("Could not retrieve persistent slices..")
+                this.debug(err.stack)
             }
             /* come up with something decent */
             this.pslices = this.slicenames.map(
@@ -159,9 +159,9 @@ export class PersistentSlices {
                                      color: generator.random_color(slicename),
                                      mine: true,
                                      current: (i == 0),
-                                   }));
+                                   }))
         }
-        this.debug("after retrieve", this.pslices);
+        this.debug("after retrieve", this.pslices)
 
     }
 
@@ -176,14 +176,14 @@ export class PersistentSlices {
         // filter out old slices
         this.pslices = this.pslices.filter(
             ps => this.slicenames.indexOf(ps.name) >= 0
-        );
+        )
         // add new slices
-        let generator = this.colors_generator;
+        let generator = this.colors_generator
         // compute the entries not yet in this.pslices
         let known = new Map(this.pslices.map(
-            pslice =>  ([pslice.name, pslice])));
+            pslice =>  ([pslice.name, pslice])))
         for (let slicename of this.slicenames) {
-            let pslice = known.get(slicename);
+            let pslice = known.get(slicename)
             if (! pslice) {
                 pslice = {
                     name: slicename,
@@ -191,68 +191,68 @@ export class PersistentSlices {
                     mine: true,
                     current: false,
                 }
-                this.pslices.push(pslice);
+                this.pslices.push(pslice)
             }
-            pslice.mine = true;
+            pslice.mine = true
         }
         // keep sorted
-        this.pslices.sort( (ps1, ps2) => (ps1.name <= ps2.name) ? -1 : 1);
+        this.pslices.sort( (ps1, ps2) => (ps1.name <= ps2.name) ? -1 : 1)
         // have we one current ?
-        let nb_currents = this.pslices.filter( ps => ps.current).length;
+        let nb_currents = this.pslices.filter( ps => ps.current).length
         if (nb_currents != 1) {
             this.pslices.forEach(function(ps, i) {
-                ps.current = (i==0);
-            });
+                ps.current = (i==0)
+            })
         }
     }
 
     // store this.pslices as-in in localStorage
     _store() {
         if (this.warn_missing_storage()) {
-            return;
+            return
         }
-        let json =  JSON.stringify(this.pslices);
+        let json =  JSON.stringify(this.pslices)
         localStorage.setItem(this.storage_key, json)
-        this.debug(`wrote in localStorage ${json}`);
+        this.debug(`wrote in localStorage ${json}`)
     }
 
     // useful when someone joins a slice that was formerly
-    // a foreign slice; its color remains gray-like
+    // a foreign slice, its color remains gray-like
     // but clearing this instance allows to re-attach colors to slices
     _clear() {
-        localStorage.setItem(this.storage_key, "");
+        localStorage.setItem(this.storage_key, "")
     }
 
     ////////// make sure the slice is known and create otherwise
     record_slice(slicename, mine=false, current=false) {
-        let existing = this._get_pslice(slicename);
+        let existing = this._get_pslice(slicename)
         if (existing)
-            return existing;
+            return existing
         let pslice = {
             name: slicename,
             mine: mine,
             current: current,
             color: this.colors_generator.random_color(slicename, mine),
-        };
-        this.pslices.push(pslice);
-        this._store();
+        }
+        this.pslices.push(pslice)
+        this._store()
         this.debug(`recorded ${slicename}`, pslice)
-        return pslice;
+        return pslice
     }
 
     ////////// return a pslice
     _get_pslice(slicename) {
         return this.pslices
-            .filter(pslice => pslice.name == slicename).first();
+            .filter(pslice => pslice.name == slicename).first()
     }
     _get_current_pslice() {
         return this.pslices
-            .filter(pslice => pslice.current).first();
+            .filter(pslice => pslice.current).first()
     }
 
     //////////
     all_slice_names() {
-        return this.pslices.map(pslice => pslice.name);
+        return this.pslices.map(pslice => pslice.name)
     }
     my_slices_names() {
         return this.pslices
@@ -262,26 +262,26 @@ export class PersistentSlices {
 
     ////////// easier access
     get_current_slice_name() {
-        let pslice = this._get_current_pslice();
-        return pslice ? pslice.name : 'user-not-in-any-slice';
+        let pslice = this._get_current_pslice()
+        return pslice ? pslice.name : 'user-not-in-any-slice'
     }
     get_current_slice_color() {
-        let pslice = this._get_current_pslice();
-        return pslice ? pslice.color : this.foreign_color;
+        let pslice = this._get_current_pslice()
+        return pslice ? pslice.color : this.foreign_color
     }
     get_slice_color(slicename) {
-        let pslice = this._get_pslice(slicename);
-        return pslice ? pslice.color : this.foreign_color;
+        let pslice = this._get_pslice(slicename)
+        return pslice ? pslice.color : this.foreign_color
     }
 
     set_current(slicename) {
-        let pslice = this._get_pslice(slicename);
+        let pslice = this._get_pslice(slicename)
         if ( ! pslice) {
-            return;
+            return
         }
-        this.pslices.forEach( pslice => pslice.current = false);
-        pslice.current = true;
-        this._store();
+        this.pslices.forEach( pslice => pslice.current = false)
+        pslice.current = true
+        this._store()
     }
 
 }
