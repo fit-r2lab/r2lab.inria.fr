eaa8815fb4adb27dae0aa4d0d2548816b3e2c9a7
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-17 15:16:58 +0100

    colons in r2lab-diff

--- assets/r2lab/r2lab-diff.js
+++ assets/r2lab/r2lab-diff.js
@@ -7,7 +7,7 @@
 /*eslint no-unused-vars: ["error", { "varsIgnorePattern": "r2lab_diff" }]*/
 
 
-"use strict";
+"use strict"
 
 // protocol is as follows
 // each codediff is assigned an id by in the markdown source
@@ -24,28 +24,28 @@
 // see markdown/views.md to see how this is used to create the tuto pages
 
 function r2lab_diff(id, lang) {
-    let a      = document.getElementById(id + '_a');
-    let b      = document.getElementById(id + '_b');
-    let diff = document.getElementById(id + '_diff');
-    let style = 'diffLines'; // also available are diffChars and diffWords
-    let jsdiff = JsDiff[style](a.textContent, b.textContent);
+    let a      = document.getElementById(id + '_a')
+    let b      = document.getElementById(id + '_b')
+    let diff = document.getElementById(id + '_diff')
+    let style = 'diffLines' // also available are diffChars and diffWords
+    let jsdiff = JsDiff[style](a.textContent, b.textContent)
 
-    let fragment = document.createDocumentFragment();
+    let fragment = document.createDocumentFragment()
     for (let i=0; i < jsdiff.length; i++) {
 	if (jsdiff[i].added && jsdiff[i + 1] && jsdiff[i + 1].removed) {
-	    let swap = jsdiff[i];
-	    jsdiff[i] = jsdiff[i + 1];
-	    jsdiff[i + 1] = swap;
+	    let swap = jsdiff[i]
+	    jsdiff[i] = jsdiff[i + 1]
+	    jsdiff[i + 1] = swap
 	}
 	let type =   (jsdiff[i].removed) ? 'del'
-	    : (jsdiff[i].added) ? 'ins' : 'code';
-	let node = document.createElement(type);
-	node.appendChild(document.createTextNode(jsdiff[i].value));
-	fragment.appendChild(node);
+	    : (jsdiff[i].added) ? 'ins' : 'code'
+	let node = document.createElement(type)
+	node.appendChild(document.createTextNode(jsdiff[i].value))
+	fragment.appendChild(node)
 	// passing e.g. lang='python' will enable prism
 	// at least on the <code> tag
-	if (lang) $(node).addClass('language-' + lang);
+	if (lang) $(node).addClass('language-' + lang)
     }
-    diff.textContent = '';
-    diff.appendChild(fragment);
+    diff.textContent = ''
+    diff.appendChild(fragment)
 }
