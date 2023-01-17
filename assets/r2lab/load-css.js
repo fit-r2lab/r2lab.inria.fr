7b361332f68eb18ace187c84778e639fccfc65be
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-17 15:11:00 +0100

    colons and formatted load-css

--- assets/r2lab/load-css.js
+++ assets/r2lab/load-css.js
@@ -2,12 +2,12 @@
 /*global $ */
 
 export function load_css(url) {
-    // I'd have expected jQuery to come with this, but apparently no..
-    $(function() {
-        $("<link/>", {
-            rel: "stylesheet",
-            type: "text/css",
-            href: url
-        }).appendTo("head");
-    })
+  // I'd have expected jQuery to come with this, but apparently no..
+  $(function () {
+    $("<link/>", {
+      rel: "stylesheet",
+      type: "text/css",
+      href: url
+    }).appendTo("head")
+  })
 }
