4303e1749e8be24037279dcd55a408a1368e5f5f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-10-28 15:15:49 +0100

    cleanup

--- md/urls.py
+++ md/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 # xxx not used in fact, see r2lab/urls.py that manages this directly
 urlpatterns = [
-#    re_path('^(?P<markdown_file>.*)$', views.markdown_page, name='show'),
-    ]
+]
